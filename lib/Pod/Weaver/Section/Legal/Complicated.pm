2c501ae7f62679597250974d0de1291864a0782e
Carnë Draug <carandraug+dev@gmail.com>
2017-01-31 19:10:41 +0000

    maint: update copyright year

--- lib/Pod/Weaver/Section/Legal/Complicated.pm
+++ lib/Pod/Weaver/Section/Legal/Complicated.pm
@@ -1,7 +1,7 @@
 package Pod::Weaver::Section::Legal::Complicated;
 
 use utf8;
-## Copyright (C) 2013 Carnë Draug <carandraug+dev@gmail.com>
+## Copyright (C) 2013-2017 Carnë Draug <carandraug+dev@gmail.com>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
